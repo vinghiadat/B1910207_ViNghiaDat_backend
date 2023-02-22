e51189f7a81d5b5634f4d81233d3d02959dcba85
ViNghiaDat <Datb1910207@student.ctu.edu.vn>
2023-02-22 09:26:37 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const contactsRouter = require("./app/routes/contact.route");
-
+const ApiError = require("./app/api-error");
 const cors = require("cors");
 
 
 const app = express();
 
 app.use("/api/contacts",contactsRouter);
+app.use((req,res,next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err,req,res,next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 app.use(cors());
 app.use(express.json());
 app.get("/", (reg, res)=> {
