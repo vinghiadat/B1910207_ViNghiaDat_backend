9773b1cffa4f23563e6d9f2779cb3fcd76c8f032
ViNghiaDat <Datb1910207@student.ctu.edu.vn>
2023-02-22 09:07:35 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const contactsRouter = require("./app/routes/contact.route");
+
 const cors = require("cors");
 
 
 const app = express();
 
-
+app.use("/api/contacts",contactsRouter);
 app.use(cors());
 app.use(express.json());
 app.get("/", (reg, res)=> {
---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (reg, res) => {
+    res.send({message :"create handle"});
+};
+
+exports.findAll = (reg, res) => {
+    res.send({message :"findAll handle"});
+};
+
+exports.findOne = (reg, res) => {
+    res.send({message :"findOne handle"});
+};
+
+exports.update = (reg, res) => {
+    res.send({message :"update handle"});
+};
+
+exports.delete = (reg, res) => {
+    res.send({message :"delete handle"});
+};
+
+exports.deteleAll = (reg, res) => {
+    res.send({message :"deteleAll handle"});
+};
+
+exports.findAllFavorite = (reg, res) => {
+    res.send({message :"findAllFavorite handle"});
+};
